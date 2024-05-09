e39b87233566b2284e2a0bb553a95c026b883eb8
PriyadarshiniNM <109581074+PriyadarshiniNM@users.noreply.github.com>
2024-05-09 13:17:42 +0530

    Update JK_FF.v

--- System verilog/JK_FF/JK_FF.v
+++ System verilog/JK_FF/JK_FF.v
@@ -1,42 +1,17 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.05.2024 12:21:35
-// Design Name: 
-// Module Name: JK_FF
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-//Design Under Test JK flip flop ( JK _FF.v )
 module JK _FF(input clk, rst, J, K,
 ourtput Q);
 reg [1:0] JK;
 JK ={J,K};
-always@(posedge clk, posedge rst)
+always@(posedge clk or posedge rst)
 begin
     if (rst)
    	 Q <= 1'b0;
     else
-    	if JK == 2'b00
-            Q <= r1.Q;
-   	 	else if JK == 2'b01
-            Q <= 1'b0;
-   	 	else if JK == 2'b10
-            Q <= 1'b1;
-   	 	else if JK == 2'b11
-            Q <= (~Q);
+        begin
+    	if JK == 2'b00                 Q <= Q;
+   	 	else if JK == 2'b01            Q <= 1'b0;
+   	 	else if JK == 2'b10            Q <= 1'b1;
+   	 	else if JK == 2'b11            Q <= (~Q);
+        end
 end
 endmodule

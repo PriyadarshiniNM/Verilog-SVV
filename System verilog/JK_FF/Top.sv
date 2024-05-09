6792ee7dfb5239526602fb80f4d790a24a0870e2
PriyadarshiniNM <109581074+PriyadarshiniNM@users.noreply.github.com>
2024-05-09 13:10:06 +0530

    Update Top.sv

--- System verilog/JK_FF/Top.sv
+++ System verilog/JK_FF/Top.sv
@@ -1,14 +1,14 @@
 //Top module of JK flip flop ( Top.sv )
 module Top ();
-bit clk;
-JK_inter I1 (clk );
-JK_FF_tb Inst1( I1.TB );
-JK_FF DUT ( I1 );
+	bit clk;
+    always #5 clk = ~clk; 
+	JK_inter I1 (clk);
+    JK_FF_tb Inst1(I1);
+    JK_FF DUT (.clk(clk), rst(I1.rst), .J(I1.J), .K(I1.K), .Q(I1,Q));
 
-always #5 clk = ~clk;
-initial begin 
-    $dumpfile( “JK_FF.vcd” );
-    $dumpvars;
-    $monitor( $time, “reset = %d, J = %d, K = %d, Q = %d”, I1.rst, I1.T, I1.Q );
-end
+    initial begin 
+        $dumpfile("JK_FF.vcd");
+        $dumpvars;
+        //$monitor( $time, "clk = %d, reset = %d, J = %d, K = %d, Q = %d", I1.rst, I1.J, I1.K, I1.Q);
+    end
 endmodule

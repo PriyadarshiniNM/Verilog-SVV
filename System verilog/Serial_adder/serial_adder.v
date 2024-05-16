cd4fded09d34b1f44d1d149fd5a46c9f53e5b7e2
Priyadarshini N M <priyadarshininm.ec21@bmsce.ac.in>
2024-05-16 19:25:24 +0530

    Updating serial_adder.v

--- System verilog/Serial_adder/serial_adder.v
+++ System verilog/Serial_adder/serial_adder.v
@@ -1,51 +1,60 @@
 module FullAdder(
-    input A, B, Cin,
-    output Sum, Cout
+    input a, b, cin,
+    output sum, cout
 );
 
-assign Sum = A ^ B ^ Cin;
-assign Cout = (A & B) | (A & Cin) | (B & Cin); 
+assign sum = a ^ b ^ cin;
+assign cout = (a & b) | (cin & (a ^ b));
 
 endmodule
 
-//PISO
-module Shift_Reg(
-    input clk, reset, 
-    input [3:0] parallel_in, 
+module PISO(
+    input clk, reset,
+    input [3:0] parallel_in,
     output reg serial_out
 );
 
-reg [3:0] shift_reg; // Shift register for parallel data
-reg [3:0] serial_out_reg; // Register for serial output
+reg [3:0] shift_reg;
+reg [3:0] serial_out_reg;
+reg [2:0] counter;
 
-always @(posedge clk or posedge reset) begin
+always @(posedge clk) begin
     if (reset) begin
-        shift_reg <= 4'b0000; // Reset the shift register
-        serial_out_reg <= 4'b0000; // Reset the serial output register
+        shift_reg <= 4'd0;
+        serial_out_reg <= 4'd0;
+        counter <= 3'd0;
+    end 
+    else if (counter == 3'b000) begin
+        shift_reg <= {shift_reg[2:0], parallel_in[0]};
+        serial_out_reg <= shift_reg;
+        counter <= 3'b0001;
     end 
     else begin
-        shift_reg <= {shift_reg[2:0], parallel_in[0]}; // Shift in the parallel input
-        serial_out_reg <= {serial_out_reg[2:0], shift_reg[3]}; // Shift out the serial output
+        shift_reg <= {shift_reg[2:0], parallel_in[0]};
+        serial_out_reg <= shift_reg;
+        counter <= counter + 1'b1;
     end
 end
 
-assign serial_out = serial_out_reg[3]; // Assign serial_out to the most significant bit of serial_out_reg
+assign serial_out = serial_out_reg[3];
 
 endmodule
 
 module SerialAdder(
-    input clk, reset,
-    input [3:0] A, B,
-    output [3:0] Sum
+    input clk,
+    input reset,
+    input [3:0] a, b,
+    output reg [3:0] result
 );
 
-wire [3:0] carry; // Carry bits for each stage
+wire [3:0] sum;
+wire [3:0] carry;
 
-FullAdder FA0(A[0], B[0], 1'b0, Sum[0], carry[0]);
-FullAdder FA1(A[1], B[1], carry[0], Sum[1], carry[1]);
-FullAdder FA2(A[2], B[2], carry[1], Sum[2], carry[2]);
-FullAdder FA3(A[3], B[3], carry[2], Sum[3], carry[3]);
+FullAdder FA0(.a(a[0]), .b(b[0]), .cin(1'b0), .sum(sum[0]), .cout(carry[0]));
+FullAdder FA1(.a(a[1]), .b(b[1]), .cin(carry[0]), .sum(sum[1]), .cout(carry[1]));
+FullAdder FA2(.a(a[2]), .b(b[2]), .cin(carry[1]), .sum(sum[2]), .cout(carry[2]));
+FullAdder FA3(.a(a[3]), .b(b[3]), .cin(carry[2]), .sum(sum[3]), .cout(result[3]));
 
-Shift_Reg piso(clk, reset, Sum, Sum[3]);
+PISO shift_reg(.clk(clk), .reset(reset), .parallel_in(sum), .serial_out());
 
 endmodule

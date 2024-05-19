16af0aaf2013c0a85f1d89859003ae6476bd7b64
Priyadarshini N M <priyadarshininm.ec21@bmsce.ac.in>
2024-05-19 18:19:01 +0530

    Updating JK_FF.v

--- System verilog/JK_FF/JK_FF.v
+++ System verilog/JK_FF/JK_FF.v
@@ -6,18 +6,18 @@ always @(posedge clk or posedge rst) begin
         Q <= 1'b0;
     end 
 	else begin
-            if (J == 1'b0 && K == 1'b0) begin
-                Q <= Q;
-            end 
+        if (J == 1'b0 && K == 1'b0) begin
+            Q <= Q;
+        end 
 	    else if (J == 1'b0 && K == 1'b1) begin
-                Q <= 1'b0;
-            end 
+            Q <= 1'b0;
+        end 
 	    else if (J == 1'b1 && K == 1'b0) begin
-                Q <= 1'b1;
-            end 
+            Q <= 1'b1;
+        end 
 	    else if (J == 1'b1 && K == 1'b1) begin
-                Q <= ~Q;  
-            end
-    	end
+            Q <= ~Q;  
+        end
+    end
 end
 endmodule

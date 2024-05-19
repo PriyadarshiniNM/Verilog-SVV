ac25f39eee712e1b978275497b81d74494efc8e9
PriyadarshiniNM <109581074+PriyadarshiniNM@users.noreply.github.com>
2024-05-19 18:23:45 +0530

    Update JK_FF.v

--- System verilog/JK_FF/JK_FF.v
+++ System verilog/JK_FF/JK_FF.v
@@ -2,22 +2,22 @@ module JK_FF (input clk, rst, J, K,
     output reg Q);
 
 always @(posedge clk or posedge rst) begin
-    if (rst) begin
+	if (rst) begin
         Q <= 1'b0;
-    end 
+    	end 
 	else begin
-        if (J == 1'b0 && K == 1'b0) begin
-            Q <= Q;
-        end 
-	    else if (J == 1'b0 && K == 1'b1) begin
-            Q <= 1'b0;
-        end 
-	    else if (J == 1'b1 && K == 1'b0) begin
-            Q <= 1'b1;
-        end 
-	    else if (J == 1'b1 && K == 1'b1) begin
-            Q <= ~Q;  
-        end
-    end
+        	if (J == 1'b0 && K == 1'b0) begin
+            		Q <= Q;
+        	end 
+		else if (J == 1'b0 && K == 1'b1) begin
+        	    Q <= 1'b0;
+		end 
+		else if (J == 1'b1 && K == 1'b0) begin
+            		Q <= 1'b1;
+       		end
+		else if (J == 1'b1 && K == 1'b1) begin
+            		Q <= ~Q;  
+        	end
+    	end
 end
 endmodule
